e025db1f00f7c134886f83a1c3249b41146abd71
omerfaruksen <sen.faruk.93@gmail.com>
2024-02-24 15:36:37 +0300

    Update questions.jsx

--- src/questions.jsx
+++ src/questions.jsx
@@ -1,11 +1,11 @@
-import cinSeddi from '../assets/cin-seddi.jpg';
+
 
 export const questions = [
   {
     question: "Çin Seddini oluşturan taşlar birbirine ne ile tutturulmuştur?",
     options: ["Bambu Harcı", "Anne Duası", "Pirinç Unu", "Noodle"],
     answer: "Pirinç Unu",
-    media: cinSeddi,
+    media: './src/assets/cin-seddi.jpg',
   },
   {
     question: "İlk Pamuk şekeri bulan kişinin mesleği nedir?",
